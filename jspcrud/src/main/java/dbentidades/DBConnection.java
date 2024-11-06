d7242cc39d2fda9f957c84912c5f3353f434b996
ronardj01 <ronardj01@gmail.com>
2024-11-06 11:52:38 +0100

    create DBConnection class to stablish connections

---
+++ jspcrud/src/main/java/dbentidades/DBConnection.java
@@ -0,0 +1,28 @@
+package dbentidades;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost/jspcrud?userSSL=false&allowPublicKeyRetrieval=true";
+    private static final String USER = System.getenv("DB_USER_PRUEBA");
+    private static final String PASS = System.getenv("DB_PASS_PRUEBA");
+    
+    public static Connection getConnection(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("¡La conexión se ha establecido correctamente!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("¡La conexión no se ha podido establecer correctamente!");
+        }
+        return conn;
+    }
+}
+
+
