63573c2778a0e42289113e885fbb5ec353dae0f5
ronardj01 <ronardj01@gmail.com>
2024-11-06 13:18:20 +0100

    crear getAllChar() metodo

---
+++ jspcrud/src/main/java/dbentidades/DBPersonaje.java
@@ -0,0 +1,29 @@
+package dbentidades;
+
+import entidades.Personaje;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public abstract class DBPersonaje {
+
+    //Insertar personaje en la base de Datos
+    public static ArrayList<Personaje> getAllChar() throws SQLException {
+        ArrayList<Personaje> personajes = new ArrayList<>();
+        String query = "SELECT id, nombre FROM personajes";
+        Connection conn = DBConnection.getConnection();
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery(query);
+        
+        while (rs.next()) {
+            Personaje personaje = new Personaje();
+            personaje.setId(rs.getInt("id"));
+            personaje.setNombre(rs.getString("nombre"));
+            personajes.add(personaje);
+        }
+       
+        return personajes;
+    }
+}
