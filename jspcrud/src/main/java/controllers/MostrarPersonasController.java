94affa043f3938266c6182338d5c8b896ea915bf
ronardj01 <ronardj01@gmail.com>
2024-11-07 10:13:43 +0100

    compartir

---
+++ jspcrud/src/main/java/controllers/MostrarPersonasController.java
@@ -0,0 +1,39 @@
+
+package controllers;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MostrarPersonasController", urlPatterns = {"/MostrarPersonasController"})
+public class MostrarPersonasController extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+    }
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
